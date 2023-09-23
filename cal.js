fe425fce7d26937837662c92cd75d90d92857797
Om Kinge <omkinge93@gmail.com>
2023-09-23 15:22:35 +0530

    first commit

---
+++ cal.js
@@ -0,0 +1,21 @@
+//Displays Values On screen
+function dis(value) {
+    document.getElementById("screen").value += value;
+}
+
+// For Evaluation
+function solve() {
+    let x = document.getElementById("screen").value;
+    let y = eval(x);
+    document.getElementById("screen").value = y;
+}
+
+//for clearing the screen
+function clr() {
+    document.getElementById("screen").value = ""
+}
+
+function backspace() {
+    let screen = document.getElementById("screen").value;
+    document.getElementById("screen").value = screen.substring(0,screen.length - 1);
+}
